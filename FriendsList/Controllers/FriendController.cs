5f66ba44f6b804a27085d3e8a3e9de6277cd55e9
JuliaTitto87 <133301250+JuliaTitto87@users.noreply.github.com>
2024-01-13 19:00:48 +0300

    I created project, added model Friend, controller FriendController, View Index

---
+++ FriendsList/Controllers/FriendController.cs
@@ -0,0 +1,87 @@
+﻿using FriendsList.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsList.Controllers
+{
+    public class FriendController : Controller
+    {
+        static List<FriendViewModel> Friends = new List<FriendViewModel>();
+        // GET: FriendController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: FriendController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FriendController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FriendController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FriendViewModel friend )
+        {
+            try
+            {
+                friend.Id=Friends.Count;
+                Friends.Add(friend);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FriendController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FriendController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(FriendViewModel friend)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FriendController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FriendController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
