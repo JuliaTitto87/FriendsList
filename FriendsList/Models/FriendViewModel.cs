91e5670aa903857f2310f39803d311babe0af3bb
JuliaTitto87 <133301250+JuliaTitto87@users.noreply.github.com>
2024-01-13 19:00:48 +0300

    Добавьте файлы проекта.

---
+++ FriendsList/Models/FriendViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FriendsList.Models
+{
+    public class FriendViewModel
+    {
+        public int Id { get; set; }
+
+        public required string FriendName { get; set; }
+
+        public int Place { get; set; }
+    }
+}
